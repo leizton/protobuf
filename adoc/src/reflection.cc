4833e62d3bfc3f75bc76b10630e90485bb91c832
leizton <leizton@126.com>
2020-04-20 16:09:11 +0800

    reflection

--- adoc/src/reflection.cc
+++ adoc/src/reflection.cc
@@ -1,12 +1,20 @@
-// @[generated_message_reflection.cc:01]
-Reflection::SetField<T>(Message* msg, FieldDescriptor* fe, T& v) {
-  T* val = MutableRaw<T>(msg, fe)
+class Reflection {
+  comment() {
+    声明在 message.h
+    实现在 generated_message_reflection.cc
+  }
+  fields() {
+    schema_  const ReflectionSchema
+  }
+  MutableRaw<T>(Message* msg, FieldDescriptor* fe) {
     uint32_t offset = schema_.GetFieldOffset(fe)
     return reinterpret_cast<T*>(reinterpret_cast<char*>(msg) + offset)
-  *val = v
-}
-
-// @[generated_message_reflection.cc:02]
-Reflection::AddField<T>(Message* msg, FieldDescriptor* fe, T& v) {
-  MutableRaw<RepeatedField<T>>(msg, fe)->Add(v)
+  }
+  SetField<T>(Message* msg, FieldDescriptor* fe, T& v) {
+    T* t = MutableRaw<T>(msg, fe)
+    *t = v
+  }
+  AddField<T>(Message* msg, FieldDescriptor* fe, T& v) {
+    MutableRaw<RepeatedField<T>>(msg, fe)->Add(v)
+  }
 }
